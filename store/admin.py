77790723f03ffdd485a891a52364f9b00ceadd60
Samson Songare <songaresamson@gmail.com>
2026-01-17 02:57:11 +0300

    customizing models list pages in admin panel by adding filters,links,custom actions also added the CollectionAdmin

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,33 @@
-from django.contrib import admin
+from django.contrib import admin,messages
+from django.db.models import QuerySet
+
 from . import models
+from django.utils.html import format_html,urlencode
+from django.db.models import Count
+from django.urls import reverse
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'Inventory'
+    parameter_name = 'inventry'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10','Low')
+        ]
+    
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt = 10) 
+    
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    actions = ['clear_inventory']
     list_display =['title','unit_price','inventory_status','collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
     list_select_related =['collection']
+    list_filter = ['collection','last_update',InventoryFilter]
 
     def collection_title(self,product):
         return product.collection.title
@@ -17,16 +38,60 @@ class ProductAdmin(admin.ModelAdmin):
             return "Low"
         return "Ok"
     
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self,request,queryset):
+        updated_count = queryset.update(inventory=0)    
+        self.message_user(
+            request,
+            f'{updated_count} products were successfully updated',
+            messages.SUCCESS
+        )
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name','last_name','membership']
+    list_display = ['first_name','last_name','membership','orders']
     list_editable =['membership']
     list_per_page = 10
     ordering = ['first_name','last_name']
+    search_fields = ['first_name__istartswith','last_name__istartswith']
+
+    @admin.display(ordering="orders")
+    def orders(self,customer):
+        url = (reverse('admin:store_order_changelist')
+                + '?' + urlencode({'customer__id':str(customer.id)})    
+               )
+        return format_html('<a href={}>{} orders</a>',url,customer.orders)
+         
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders = Count('order')
+        )
+
+
+
 
 # registering collection model
-admin.site.register(models.Collection)
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title','products_count']
+    @admin.display(ordering='products_count')
+
+    def products_count(self,collection):
+        url =( reverse('admin:store_product_changelist')
+              + "?" + urlencode(
+                  {
+                      'collection__id': str(collection.id)
+                  }
+              )
+              )
+        return format_html('<a href ="{}">{}</a>',url,collection.products_count)
+         
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count = Count('product')
+        )
 
 # registering order model
 @admin.register(models.Order)
