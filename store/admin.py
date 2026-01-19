b25946a6ad1d21deee6c421d0df65209beb152cd
Samson Songare <songaresamson@gmail.com>
2026-01-20 01:37:29 +0300

    decoupled the store app from tags app by creating custom_store

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,9 @@
-from django.contrib import admin,messages
-from django.contrib.contenttypes.admin import GenericTabularInline
+from django.contrib import admin, messages
 from django.db.models import QuerySet
-
 from . import models
-from django.utils.html import format_html,urlencode
+from django.utils.html import format_html, urlencode
 from django.db.models import Count
 from django.urls import reverse
-from tags.models import TagItem
 
 
 class InventoryFilter(admin.SimpleListFilter):
@@ -15,108 +12,98 @@ class InventoryFilter(admin.SimpleListFilter):
 
     def lookups(self, request, model_admin):
         return [
-            ('<10','Low')
+            ('<10', 'Low')
         ]
-    
+
     def queryset(self, request, queryset):
         if self.value() == '<10':
-            return queryset.filter(inventory__lt = 10) 
-    
-class TagInline(GenericTabularInline):
-    autocomplete_fields = ['tag']
-    extra=0
-    min_num=1
-    max_num=10
-    model = TagItem
+            return queryset.filter(inventory__lt=10)
+
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    prepopulated_fields ={
-        'slug':['title']
+    prepopulated_fields = {
+        'slug': ['title']
     }
     autocomplete_fields = ['collection']
     actions = ['clear_inventory']
-    inlines =[TagInline]
-    list_display =['title','unit_price','inventory_status','collection_title']
+    list_display = ['title', 'unit_price',
+                    'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
-    list_select_related =['collection']
-    list_filter = ['collection','last_update',InventoryFilter]
+    list_select_related = ['collection']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     search_fields = ['title']
 
-    def collection_title(self,product):
+    def collection_title(self, product):
         return product.collection.title
-    
+
     @admin.display(ordering='inventory')
-    def inventory_status(self,product):
-        if product.inventory <10:
+    def inventory_status(self, product):
+        if product.inventory < 10:
             return "Low"
         return "Ok"
-    
+
     @admin.action(description='Clear inventory')
-    def clear_inventory(self,request,queryset):
-        updated_count = queryset.update(inventory=0)    
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
         self.message_user(
             request,
             f'{updated_count} products were successfully updated',
             messages.SUCCESS
         )
 
+
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name','last_name','membership','orders']
-    list_editable =['membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders']
+    list_editable = ['membership']
     list_per_page = 10
-    ordering = ['first_name','last_name']
-    search_fields = ['first_name__istartswith','last_name__istartswith']
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
     @admin.display(ordering="orders")
-    def orders(self,customer):
+    def orders(self, customer):
         url = (reverse('admin:store_order_changelist')
-                + '?' + urlencode({'customer__id':str(customer.id)})    
+               + '?' + urlencode({'customer__id': str(customer.id)})
                )
-        return format_html('<a href={}>{} orders</a>',url,customer.orders)
-         
-    
+        return format_html('<a href={}>{} orders</a>', url, customer.orders)
+
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
-            orders = Count('order')
+            orders=Count('order')
         )
-
-
 
 
 # registering collection model
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ['title','products_count']
+    list_display = ['title', 'products_count']
     search_fields = ['title']
-    @admin.display(ordering='products_count')
 
-    def products_count(self,collection):
-        url =( reverse('admin:store_product_changelist')
-              + "?" + urlencode(
-                  {
-                      'collection__id': str(collection.id)
-                  }
-              )
-              )
-        return format_html('<a href ="{}">{}</a>',url,collection.products_count)
-         
-    
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = (reverse('admin:store_product_changelist')
+               + "?" + urlencode(
+            {
+                'collection__id': str(collection.id)
+            }
+        )
+        )
+        return format_html('<a href ="{}">{}</a>', url, collection.products_count)
+
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
-            products_count = Count('product')
+            products_count=Count('product')
         )
-    
+
+
 class OrderItemInline(admin.TabularInline):
     autocomplete_fields = ['product']
-    min_num=1
-    max_num=10
-    extra=0
+    min_num = 1
+    max_num = 10
+    extra = 0
     model = models.OrderItem
-
-   
 
 
 # registering order model
@@ -124,7 +111,5 @@ class OrderItemInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     autocomplete_fields = ['customer']
     inlines = [OrderItemInline]
-    list_per_page =10
-    list_display =['id','placed_at','customer']
-   
-
+    list_per_page = 10
+    list_display = ['id', 'placed_at', 'customer']
--- store_custom/admin.py
+++ store_custom/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from store.admin import ProductAdmin
+from tags.models import TagItem
+from django.contrib.contenttypes.admin import GenericTabularInline
+from store.models import Product
 
-# Register your models here.
+
+class TagInline(GenericTabularInline):
+    autocomplete_fields = ['tag']
+    extra = 0
+    min_num = 1
+    max_num = 10
+    model = TagItem
+
+
+class CustomProductAdmin(ProductAdmin):
+    inlines = [TagInline]
+
+
+admin.site.unregister(Product)
+admin.site.register(Product, CustomProductAdmin)
