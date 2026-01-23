daf194109ed49b9b3152ef4d73ec19aa6385b115
Samson Songare <songaresamson@gmail.com>
2026-01-23 13:04:45 +0300

    using model serializers in product serializer and added post,put and delete method for product

--- store/serializers.py
+++ store/serializers.py
@@ -1,25 +1,23 @@
 from rest_framework import serializers
-from .models import Product,Collection
+from .models import Product, Collection
 from decimal import Decimal
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6,decimal_places=2,source='unit_price')
-    price_with_tax = serializers.SerializerMethodField(method_name='calculated_tax')
-    # collection =serializers.PrimaryKeyRelatedField(
-    #     queryset = Collection.objects.all()
-    # )
-    # collection = serializers.StringRelatedField()
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name = 'collection-detail'
-    )
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
 
-    def calculated_tax(self,product:Product):
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title',"inventory","slug","description",
+                  'unit_price', 'price_with_tax', 'collection']
+
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculated_tax')
+
+    def calculated_tax(self, product: Product):
         return product.unit_price*Decimal(1.1)
+
--- store/views.py
+++ store/views.py
@@ -7,19 +7,37 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
-@api_view()
+@api_view(['GET','POST'])
 def product_list(request):
-    query_set = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(query_set, many=True,context={'request':request})
-    return Response(serializer.data)
+    if request.method == 'GET':
+        query_set = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(query_set, many=True,context={'request':request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+        
 
 
-@api_view()
+
+@api_view(['GET', 'PUT','DELETE'])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
-
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if product.orderitems.count() > 0:
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 @api_view()
 def collection_detail(request,pk):
     return Response('ok')
