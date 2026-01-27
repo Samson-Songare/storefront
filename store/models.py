81bf0a8a110841da6eb78426b2e85f66d673af85
Samson Songare <songaresamson@gmail.com>
2026-01-27 15:53:39 +0300

    added unique for cart and product field in cartitem model

--- store/models.py
+++ store/models.py
@@ -114,9 +114,11 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE,related_name="items")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
+    class Meta:
+        unique_together = [['cart','product']]
 
 
 # reviw model
