8b13d8ca55d5ba9f8635ece07703ca46ae24f344
Samson Songare <songaresamson@gmail.com>
2026-01-27 13:06:22 +0300

    created review model ,its serializer,implented nested route for review and also implemented products filtering based on its id

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,15 @@
 from . import views
-from rest_framework.routers import SimpleRouter,DefaultRouter
+from rest_framework_nested import routers
 
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet,basename='products')
+router.register('collections', views.CollectionViewSet)
 
-router = DefaultRouter()
-router.register('products',views.ProductViewSet)
-router.register('collections',views.CollectionViewSet)
-
+# creating child route
+products_router = routers.NestedDefaultRouter(
+    router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet,
+                         basename='product-review')
 
 # url configuration
-urlpatterns = router.urls
+urlpatterns = router.urls + products_router.urls
--- store/views.py
+++ store/views.py
@@ -8,13 +8,19 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .models import Product, Collection, OrderItem
-from .serializers import ProductSerializer, CollectionSerializer
+from .models import Product, Collection, OrderItem, Review
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        collection_id = self.request.query_params.get('collection_id')
+        if collection_id is not None:
+            queryset = queryset.filter(collection_id=collection_id)
+        return queryset
 
     def get_serializer_context(self):
         return {'request': self.request}
@@ -38,4 +44,14 @@ class CollectionViewSet(ModelViewSet):
         if collection.products.count() > 0:
             return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
-  
+
+
+class ReviewViewSet(ModelViewSet):
+
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
