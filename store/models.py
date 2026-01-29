9e7d7ad5e578765bc000168226f1e1a865ee3b7d
Samson Songare <songaresamson@gmail.com>
2026-01-29 19:22:33 +0300

    created custom permission for cancelling an order

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator
@@ -71,9 +72,11 @@ class Customer(models.Model):
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
 
+    @admin.display(ordering='user__first_name')
     def first_name(self):
         return self.user.first_name
-
+    
+    @admin.display(ordering='user__last_name')
     def last_name(self):
         return self.user.last_name
 
@@ -96,6 +99,12 @@ class Order(models.Model):
     payment_status = models.CharField(
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+    class Meta:
+        permissions =[
+            ('cancel_order','Can cancel order')
+        ]
 
 
 class Address(models.Model):
