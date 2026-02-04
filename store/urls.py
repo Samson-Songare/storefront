7cce9b2f72dbda746d634761b2c013badde11b58
Samson Songare <songaresamson@gmail.com>
2026-02-04 15:40:57 +0300

    Added permission while querying oders

--- store/urls.py
+++ store/urls.py
@@ -2,11 +2,11 @@ from . import views
 from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
-router.register('products', views.ProductViewSet,basename='products')
+router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
-router.register('carts',views.CartViewSet)
-router.register('customers',views.CustomerViewSet)
-router.register('orders',views.OrderViewSet)
+router.register('carts', views.CartViewSet)
+router.register('customers', views.CustomerViewSet)
+router.register('orders', views.OrderViewSet, basename='orders')
 
 # creating child route
 products_router = routers.NestedDefaultRouter(
@@ -16,8 +16,8 @@ products_router.register('reviews', views.ReviewViewSet,
 
 # creating child router for carts
 
-cart_item_router = routers.NestedDefaultRouter(router,'carts',lookup='cart')
-cart_item_router.register('items',views.CartItemViewSet,basename='cart-item')
+cart_item_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+cart_item_router.register('items', views.CartItemViewSet, basename='cart-item')
 
 # url configuration
-urlpatterns = router.urls + products_router.urls +cart_item_router.urls
+urlpatterns = router.urls + products_router.urls + cart_item_router.urls
